26308154a426cc62443ea9f3cef8d064cdbfd6e8
Tim Dolan <timmydolan21@gmail.com>
2018-08-23 22:05:59 -0400

    unit-4-game

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,23 +7,19 @@ let guessesRemaining = 15;
 let userChoice = [""];
 
 //Generates random number for user to guess
-let minNumber = 0;
-let maxNumber = 84;
+var randomNumber = Math.floor(Math.random() * 120);
+var randomNumCrys = Math.floor(Math.random() * 25);
 
-let randomNumber = randomNumberFromRange(minNumber, maxNumber);
-
-function randomNumberFromRange(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
 console.log(randomNumber);
-//displays random number to the screen for user
-$("#randomnum").text(randomNumber)
 
+//displays random number to the screen for user
+$("#randomnum").text(randomNumber);
 
 //Register for user clicks on crystals
 $("#em").click(function() {
-    
-    
+    userChoice=randomNumCrys
+    $(".crystalValue").html("<p> Your current guess:" + userChoice + "</p>");
+    console.log(randomNumCrys);
+      
 });
-
+//if/else conditions for wins
